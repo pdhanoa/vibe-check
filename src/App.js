12782e75f82c7dfaface603dab13223842943a37
NoodleSoup0 <106336347+NoodleSoup0@users.noreply.github.com>
2022-05-28 17:17:08 -0500

    Icon errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import './App.css';
 import { Body } from './Body.js';
 import Title from './Title.js';
+import React from 'react';
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <Navigation
+            // you can use your own router's api to get pathname
+            activeItemId="/management/members"
+            onSelect={({itemId}) => {
+              // maybe push to the route
+            }}
+            items={[
+              {
+                title: 'Dashboard',
+                itemId: '/dashboard',
+                // you can use your own custom Icon component as well
+                // icon is optional
+                elemBefore: () => <Icon name="inbox" />,
+              },
+              {
+                title: 'Management',
+                itemId: '/management',
+                elemBefore: () => <Icon name="users" />,
+                subNav: [
+                  {
+                    title: 'Projects',
+                    itemId: '/management/projects',
+                  },
+                  {
+                    title: 'Members',
+                    itemId: '/management/members',
+                  },
+                ],
+              },
+              {
+                title: 'Another Item',
+                itemId: '/another',
+                subNav: [
+                  {
+                    title: 'Teams',
+                    itemId: '/management/teams',
+                  },
+                ],
+              },
+            ]}
+          />
       <Body/>
     </div>
   );
