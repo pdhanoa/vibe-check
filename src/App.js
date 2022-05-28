6ec5a36406888f47546f066c84c6434cf7c59ae0
Sydney <sydney.rae.smith@gmail.com>
2022-05-28 16:52:02 -0500

    title bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Body } from './Body.js';
+import Title from './Title.js';
 
 function App() {
   return (
     <div className="App">
+      <Title/>
       <Body/>
     </div>
   );
