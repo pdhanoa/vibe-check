5ad74d746ceb00bdd68937038b0142d6fc0188d6
NoodleSoup0 <106336347+NoodleSoup0@users.noreply.github.com>
2022-05-28 15:11:34 -0500

    Update MetricsForm.js

--- src/MetricsForm.js
+++ src/MetricsForm.js
@@ -8,14 +8,18 @@ export const MetricsForm = (props) => {
         <p>Select the metrics you would like to track</p>
         <form id="metrics-form">
             <input type="checkbox" name="steps"/>
-            <label for="vehicle1">Steps per day</label>
+            <label for="steps">Steps per day</label>
             <br/>
             <input type="checkbox" name="water"/>
-            <label for="vehicle1">Water intake</label>
+            <label for="water">Water intake</label>
             <br/>
             <input type="checkbox" name="exercise"/>
-            <label for="vehicle1">Daily exercise</label>
+            <label for="exercise">Daily exercise</label>
             <br/>
+            <input type="checkbox" name="sleep"/>
+            <label for="sleep">Sleep hours</label>
+            <br/>
+            
 
             <button onClick={() => {props.onSubmit(); props.changePages();}}>Submit</button>
         </form>
