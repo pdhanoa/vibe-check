80115d428c2953fd9b2b074835f121b346fc8215
pdhanoa <dhanoa.ne@gmail.com>
2022-05-29 12:38:50 -0500

    pushing calendar stuff???

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react' 
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 // Calendar page component
 
@@ -9,8 +11,16 @@ const Calendar = () => {
     return (
         <div>
             <FullCalendar
-                plugins={[ dayGridPlugin ]}
+                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin]}
                 initialView="dayGridMonth"
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                // weekends={this.state.weekendsVisible} // alternatively, use the `events` setting to fetch from a feed
+                // select={this.handleDateSelect}
+                // eventClick={this.handleEventClick}
+                // eventsSet={this.handleEvents} 
             />
         </div>
     )
